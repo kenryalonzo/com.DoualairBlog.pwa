d818a82adff4e098295bfd900bea1ffdf2084ee0
FOTIE FOTIE NATHANAEL <kenryalonzo@gmail.com>
2025-07-27 11:57:43 +0100

    add redux persit

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,14 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { store } from './redux/store'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor, store } from './redux/store'
 
 createRoot(document.getElementById('root')!).render(
-    <Provider store={store}>
-      <App />
+    <Provider store={store}>  
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>,
 )
